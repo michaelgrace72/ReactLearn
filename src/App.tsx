35a8d5f630eeccee5914a8b80337e9edb12c8ddc
michaelgrace72 <mikhagracia72@gmail.com>
2024-08-07 13:28:24 +0700

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["Mojokerto", "Sidoarjo", "Surabaya", "Gresik"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+    <div className="alert alert-primary">
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
